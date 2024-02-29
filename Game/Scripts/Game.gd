45741c9ceed90fa4bb970e56a13b694d22f1eab0
Nyoman Ari S <90953569+nyomanari21@users.noreply.github.com>
2024-02-29 23:01:12 +0700

    Tambah sistem timer untuk memulai hari
    Menambahkan sistem timer, ketika tombol di klik akan menjalankan timer. lalu ketika timer selesai maka uang akan bertambah

--- Game/Scripts/Game.gd
+++ Game/Scripts/Game.gd
@@ -1,21 +1,30 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var money = 0
+var money = 0 # Variabel penyimpan jumlah uang
+var timerStart = false # Variabel penyimpan status timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$LabelMoney.text = str(money)
+	$LabelMoney.text = "Rp" + str(money)
 
+# Fungsi tombol 'ButtonStartDay' ketika di klik akan memulai timer 'TimerStartDay'
+func _on_ButtonStartDay_pressed():
+	# Jika tombol di klik, jalankan timer dan nonaktifkan tombol 'ButtonStartDay'
+	if timerStart == false:
+		timerStart = true
+		$TimerStartDay.start()
+		$ButtonStartDay.disabled = true
 
-# Fungsi tombol 'ButtonTambah' ketika di klik akan menambah nilai 'money'
-func _on_ButtonTambah_pressed():
-	money += 1
+# Fungsi timer timeout 'TimerStartDay' ketika waktu selesai akan menambahkan uang
+func _on_TimerStartDay_timeout():
+	# Ketika timer selesai, matikan timer, aktifkan tombol 'ButtonStartDay', dan tambah uang sebanyak 1
+	if timerStart == true:
+		timerStart = false
+		$TimerStartDay.stop()
+		$ButtonStartDay.disabled = false
+		money += 1
