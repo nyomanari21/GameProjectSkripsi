58cd2de3ac675798458ba66e974ffbc7938607cb
Nyoman Ari S <90953569+nyomanari21@users.noreply.github.com>
2024-03-07 14:25:31 +0700

    Progres 3
    Update:
    - Membuat class Shop yang didalamnya ada variabel 'money'
    - Membuat sistem transaksi dengan frekuensi tertentu setiap kali memulai hari dengan klik tombol 'ButtonStartDay'

--- Game/Scripts/Game.gd
+++ Game/Scripts/Game.gd
@@ -1,8 +1,9 @@
-extends Control
+extends Node
 
 
-var money = 0 # Variabel penyimpan jumlah uang
-var timerStart = false # Variabel penyimpan status timer
+onready var shop = get_node("Shop") # Variabel penyimpan class Shop
+var timerStartDayStart = false # Variabel penyimpan status 'TimerStartDay'
+var timerTransactionStart = false # Variabel penyimpan status 'TimerTransaction'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,21 +11,32 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$LabelMoney.text = "Rp" + str(money)
+	$UI/LabelMoney.text = "Rp" + str(shop.get_money()) # Menampilkan uang yang dimiliki pemain saat ini
+	$TimerTransaction.wait_time = 2 # Atur frekuensi transaksi yang terjadi
 
-# Fungsi tombol 'ButtonStartDay' ketika di klik akan memulai timer 'TimerStartDay'
+# Fungsi tombol 'ButtonStartDay' ketika di klik akan memulai timer 'TimerStartDay' dan 'TimerTransaction'
 func _on_ButtonStartDay_pressed():
-	# Jika tombol di klik, jalankan timer dan nonaktifkan tombol 'ButtonStartDay'
-	if timerStart == false:
-		timerStart = true
+	# Jika tombol di klik, jalankan timer 'TimerStartDay' dan 'TimerTransaction'
+	# lalu nonaktifkan tombol 'ButtonStartDay'
+	if timerStartDayStart == false and timerTransactionStart == false:
+		timerStartDayStart = true
+		timerTransactionStart = true
 		$TimerStartDay.start()
-		$ButtonStartDay.disabled = true
+		$TimerTransaction.start()
+		$UI/ButtonStartDay.disabled = true
 
-# Fungsi timer timeout 'TimerStartDay' ketika waktu selesai akan menambahkan uang
+# Fungsi timer timeout 'TimerStartDay' ketika waktu selesai akan memberhentikan progres hari tersebut
 func _on_TimerStartDay_timeout():
-	# Ketika timer selesai, matikan timer, aktifkan tombol 'ButtonStartDay', dan tambah uang sebanyak 1
-	if timerStart == true:
-		timerStart = false
+	# Ketika timer selesai, matikan timer 'TimerStartDay' dan 'TimerTransaction', aktifkan tombol 'ButtonStartDay'
+	if timerStartDayStart == true and timerTransactionStart == true:
+		timerStartDayStart = false
+		timerTransactionStart = false
 		$TimerStartDay.stop()
-		$ButtonStartDay.disabled = false
-		money += 1
+		$TimerTransaction.stop()
+		$UI/ButtonStartDay.disabled = false
+
+# Fungsi timer timeout 'TimerTransaction' untuk mengatur frekuensi banyaknya transaksi yang terjadi
+# ketika 'ButtonStartDay' di klik dan 'TimerStartDay' berjalan
+func _on_TimerTransaction_timeout():
+	# Ketika timer selesai, akan menambahkan uang pemain
+	shop.set_money(shop.get_money()+1)
---
+++ Game/Scripts/Shop.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+var money:int = 0 setget set_money, get_money
+
+func set_money(new_money):
+	money = new_money
+	
+func get_money():
+	return money
