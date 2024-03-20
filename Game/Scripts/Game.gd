20e539c6b9922c15897d5619eb359b76792eda37
Nyoman Ari S <90953569+nyomanari21@users.noreply.github.com>
2024-03-20 15:54:56 +0700

    Progres 4
    Update:
    - Membuat variabel harga makanan
    - Membuat panel untuk mengatur toko
    - Membuat tombol untuk popup panel pengatur toko

--- Game/Scripts/Game.gd
+++ Game/Scripts/Game.gd
@@ -1,9 +1,11 @@
-extends Node
+# Class game engine, penyimpan semua fungsi game
+extends Control
 
 
 onready var shop = get_node("Shop") # Variabel penyimpan class Shop
 var timerStartDayStart = false # Variabel penyimpan status 'TimerStartDay'
 var timerTransactionStart = false # Variabel penyimpan status 'TimerTransaction'
+var panelShopSettingsOpened = true # Variabel penyimpan status 'PanelShopSettings'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,32 +13,48 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$UI/LabelMoney.text = "Rp" + str(shop.get_money()) # Menampilkan uang yang dimiliki pemain saat ini
+	$UI/LabelMoney.text = "Rp" + str(shop.getMoney()) # Menampilkan uang yang dimiliki pemain saat ini
+	$UI/PanelShopSettings/LabelFoodPrice.text = "Rp" + str(shop.getFoodPrice()) # Menampilkan harga makanan saat ini
 	$TimerTransaction.wait_time = 2 # Atur frekuensi transaksi yang terjadi
 
 # Fungsi tombol 'ButtonStartDay' ketika di klik akan memulai timer 'TimerStartDay' dan 'TimerTransaction'
 func _on_ButtonStartDay_pressed():
 	# Jika tombol di klik, jalankan timer 'TimerStartDay' dan 'TimerTransaction'
-	# lalu nonaktifkan tombol 'ButtonStartDay'
+	# lalu nonaktifkan tombol 'ButtonStartDay' dan 'ButtonShopSettings'
 	if timerStartDayStart == false and timerTransactionStart == false:
 		timerStartDayStart = true
 		timerTransactionStart = true
 		$TimerStartDay.start()
 		$TimerTransaction.start()
 		$UI/ButtonStartDay.disabled = true
+		$UI/ButtonShopSettings.disabled = true
 
 # Fungsi timer timeout 'TimerStartDay' ketika waktu selesai akan memberhentikan progres hari tersebut
 func _on_TimerStartDay_timeout():
-	# Ketika timer selesai, matikan timer 'TimerStartDay' dan 'TimerTransaction', aktifkan tombol 'ButtonStartDay'
+	# Ketika timer selesai, matikan timer 'TimerStartDay' dan 'TimerTransaction',
+	# aktifkan tombol 'ButtonStartDay' dan 'ButtonShopSettings'
 	if timerStartDayStart == true and timerTransactionStart == true:
 		timerStartDayStart = false
 		timerTransactionStart = false
 		$TimerStartDay.stop()
 		$TimerTransaction.stop()
 		$UI/ButtonStartDay.disabled = false
+		$UI/ButtonShopSettings.disabled = false
 
 # Fungsi timer timeout 'TimerTransaction' untuk mengatur frekuensi banyaknya transaksi yang terjadi
 # ketika 'ButtonStartDay' di klik dan 'TimerStartDay' berjalan
 func _on_TimerTransaction_timeout():
 	# Ketika timer selesai, akan menambahkan uang pemain
-	shop.set_money(shop.get_money()+1)
+	shop.setMoney(shop.getMoney()+1)
+
+
+# Fungsi pengatur panel PanelShopSettings
+func _on_ButtonShopSettings_pressed():
+	# Jika panel belum terbuka, ketika di klik akan membuka panel
+	if panelShopSettingsOpened == false:
+		$UI/PanelShopSettings/AnimationPlayer.play("Popup")
+		panelShopSettingsOpened = true
+	# Jika panel sudah terbuka, ketika di klik akan menutup panel
+	else:
+		$UI/PanelShopSettings/AnimationPlayer.play_backwards("Popup")
+		panelShopSettingsOpened = false
--- Game/Scripts/Shop.gd
+++ Game/Scripts/Shop.gd
@@ -1,10 +1,20 @@
+# Class data toko pemain
 extends Control
 
 
-var money:int = 0 setget set_money, get_money
+var money:int = 0 setget setMoney, getMoney # Variabel penyimpan uang toko pemain
+var foodPrice:int = 0 setget setFoodPrice, getFoodPrice # Variabel penyimpan harga makanan
 
-func set_money(new_money):
-	money = new_money
-	
-func get_money():
+# Setter dan Getter uang toko
+func setMoney(newMoney):
+	money = newMoney
+
+func getMoney():
 	return money
+
+# Setter dan Getter harga makanan
+func setFoodPrice(newFoodPrice):
+	foodPrice = newFoodPrice
+
+func getFoodPrice():
+	return foodPrice
